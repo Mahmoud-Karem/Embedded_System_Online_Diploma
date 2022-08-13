1c72948636d649b9bd6edcb343a48666d4b82a3c
Karem <hodazamel1112@gmail.com>
2022-08-13 04:15:23 +0200

    Update

---
+++ Unit_6_MCU_Fundamentals/Lec3_MCU_Clocks/LAB1_STM32F103C6/Lab1_Unit6_Lec3/Debug/Src/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/main.o: ../Src/main.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/syscalls.o: ../Src/syscalls.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/sysmem.o: ../Src/sysmem.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ Unit_6_MCU_Fundamentals/Lec3_MCU_Clocks/LAB3_STM32F103C6/Unit6_Lec3_Lab3/Src/main.c
@@ -0,0 +1,45 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    main.c    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ * Author  : Mahmoud Karem Zamel
+ *
+ */
+
+/*************************************************************/
+#include "Platform_Types.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define GPIOA_BASE  0x40010800
+#define GPIOA_CRH   *(vuint32_t *)(GPIOA_BASE + 0x04)
+#define GPIOA_ODR   *(vuint32_t *)(GPIOA_BASE + 0x0C)
+
+#define RCC_BASE    0x40021000
+#define RCC_APB2ENR *(vuint32_t *)(RCC_BASE + 0x18)
+#define RCC_CFGR    *(vuint32_t *)(RCC_BASE + 0x04)
+#define RCC_CR      *(vuint32_t *)(RCC_BASE + 0x00)
+
+
+int main(void)
+{
+    RCC_CFGR    |= (0b10<<0);       // System clock switch to PLL
+    RCC_CFGR    |= (0b0110<<18);    // PLL multiplication factor * 8
+    RCC_CFGR    |= (0b100<<8);      // Set APB1 Prescale to \2
+    RCC_CFGR    |= (0b101<<11);     // Set APB2 Prescale to \4
+    RCC_APB2ENR |= (1<<2);          // Enable Clock for GPIOA
+    RCC_CR      |= (1<<24);         // PLL Enable
+
+    /* Init GPIOA */
+    GPIOA_CRH &= 0xFF0FFFFF;
+    GPIOA_CRH |= 0x00200000;
+    while(1)
+    {
+        GPIOA_ODR |= 1<<13 ;
+        for (int i = 0; i < 5000; i++); // arbitrary delay
+        GPIOA_ODR &= ~(1<<13) ;
+        for (int i = 0; i < 5000; i++); // arbitrary delay
+    }
+}
